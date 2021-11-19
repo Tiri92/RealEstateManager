3579f17eee9abb95014ee7721a743c0de13b7cd9
Thierry Bitar <bitarthierry@gmail.com>
2021-11-19 17:51:29 +0100

    User can now pick image from camera and gallery and save them in app files

--- app/src/main/java/thierry/realestatemanager/ui/AddAndUpdateProperty.kt
+++ app/src/main/java/thierry/realestatemanager/ui/AddAndUpdateProperty.kt
@@ -1,11 +1,17 @@
 package thierry.realestatemanager.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
@@ -13,6 +19,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import thierry.realestatemanager.databinding.FragmentAddAndUpdatePropertyBinding
 import thierry.realestatemanager.model.Property
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,6 +32,7 @@ import thierry.realestatemanager.model.Property
 class AddAndUpdateProperty : Fragment() {
 
     private var _binding: FragmentAddAndUpdatePropertyBinding? = null
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -39,18 +50,43 @@ class AddAndUpdateProperty : Fragment() {
         _binding = FragmentAddAndUpdatePropertyBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
+        //GALLERY
         val imageView: ImageView = binding.imageview
         val getImage = registerForActivityResult(
             ActivityResultContracts.GetContent(),
             ActivityResultCallback {
                 imageView.setImageURI(it)
+                val fileName: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(
+                    context?.applicationContext?.contentResolver,
+                    it
+                )
+                if (bitmap != null) {
+                    savePhotoToInternalMemory("Photo_$fileName", bitmap)
+                }
             }
         )
-
         val photoButton: AppCompatButton = binding.buttonForMainPhoto
         photoButton.setOnClickListener(View.OnClickListener {
             getImage.launch("image/*")
         })
+
+        //CAMERA
+        val cameraButton: AppCompatButton = binding.buttonForMainPhotoCamera
+        cameraButton.setOnClickListener(View.OnClickListener {
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            activityResultLauncher.launch(intent)
+        })
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+                if (result!!.resultCode == Activity.RESULT_OK) {
+                    var bitmap = result!!.data!!.extras!!.get("data") as Bitmap
+                    binding.imageview.setImageBitmap(bitmap)
+                    val fileName: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                    savePhotoToInternalMemory("Photo_$fileName", bitmap)
+                } else {
+                }
+            }
 
         val recyclerView: RecyclerView = binding.recyclerviewFragmentAddAndUpdate
         val listOfPropertyPhoto = listOf<Property>(Property(price = 12), Property(price = 15))
@@ -65,6 +101,23 @@ class AddAndUpdateProperty : Fragment() {
             LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         recyclerView!!.layoutManager = myLayoutManager
         recyclerView.adapter = AddAndUpdateAdapter()
+    }
+
+    private fun savePhotoToInternalMemory(filename: String, bmp: Bitmap): Boolean {
+        return try {
+            context?.openFileOutput("$filename.jpg", Activity.MODE_PRIVATE).use { stream ->
+
+                //compress photo
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
+                    throw IOException("erreur compression")
+                }
+            }
+            true
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
     }
 
     override fun onDestroyView() {
