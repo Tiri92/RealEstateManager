bc4bd722b0498acaccde5ded4c6f446d918f1f66
Thierry Bitar <bitarthierry@gmail.com>
2021-12-09 10:21:18 +0100

    Display information from Database about the selected property

--- app/src/main/java/thierry/realestatemanager/ui/updateproperty/UpdatePropertyFragment.kt
+++ app/src/main/java/thierry/realestatemanager/ui/updateproperty/UpdatePropertyFragment.kt
@@ -5,12 +5,17 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatSpinner
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.switchmaterial.SwitchMaterial
 import dagger.hilt.android.AndroidEntryPoint
 import thierry.realestatemanager.R
@@ -23,10 +28,11 @@ class UpdatePropertyFragment : UpdatePropertyAdapter.PhotoDescriptionChanged, Fr
     private var _binding: FragmentAddUpdatePropertyBinding? = null
     private val viewModel by viewModels<UpdatePropertyViewModel>()
     private val binding get() = _binding!!
+    lateinit var resultPropertyTypeSpinner: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentAddUpdatePropertyBinding.inflate(inflater, container, false)
         val rootView = binding.root
@@ -65,6 +71,64 @@ class UpdatePropertyFragment : UpdatePropertyAdapter.PhotoDescriptionChanged, Fr
                 binding.cityEditText.setText(currentFullProperty.property.address!!.city)
                 binding.postcodeEditText.setText(currentFullProperty.property.address.postcode.toString())
                 binding.streetEditText.setText(currentFullProperty.property.address.street)
+
+                when (currentFullProperty.pointsOfInterestList[0].school) {
+                    true -> binding.schoolChip.isChecked = true
+                    false -> binding.schoolChip.isChecked = false
+                }
+
+                when (currentFullProperty.pointsOfInterestList[0].parks) {
+                    true -> binding.parksChip.isChecked = true
+                    false -> binding.parksChip.isChecked = false
+                }
+
+                when (currentFullProperty.pointsOfInterestList[0].shoppingCenter) {
+                    true -> binding.shoppingCentreChip.isChecked = true
+                    false -> binding.shoppingCentreChip.isChecked = false
+                }
+
+                when (currentFullProperty.pointsOfInterestList[0].sportsClubs) {
+                    true -> binding.sportsClubsChip.isChecked = true
+                    false -> binding.sportsClubsChip.isChecked = false
+                }
+
+                when (currentFullProperty.pointsOfInterestList[0].stations) {
+                    true -> binding.stationsChip.isChecked = true
+                    false -> binding.stationsChip.isChecked = false
+                }
+
+                when (currentFullProperty.pointsOfInterestList[0].university) {
+                    true -> binding.universityChip.isChecked = true
+                    false -> binding.universityChip.isChecked = false
+                }
+
+                val currentPropertyType = currentFullProperty.property.type
+                val propertyTypeSpinner: AppCompatSpinner = binding.typeOfPropertySpinner
+                val propertyTypeAdapter = ArrayAdapter.createFromResource(
+                    requireContext(),
+                    R.array.PropertiesTypes,
+                    android.R.layout.simple_spinner_item
+                )
+                propertyTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                propertyTypeSpinner.adapter = propertyTypeAdapter
+                propertyTypeSpinner.onItemSelectedListener =
+                    object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(
+                            parent: AdapterView<*>?,
+                            p1: View?,
+                            position: Int,
+                            p3: Long,
+                        ) {
+                            val tSpinnerResult: String =
+                                parent?.getItemAtPosition(position).toString()
+                            resultPropertyTypeSpinner = tSpinnerResult
+                        }
+
+                        override fun onNothingSelected(p0: AdapterView<*>?) {
+                        }
+                    }
+                propertyTypeSpinner.setSelection(getCurrentPropertyTypeIndex(propertyTypeSpinner,
+                    currentPropertyType))
 
                 setUpRecyclerView(recyclerView, currentFullProperty.mediaList)
             }
@@ -134,6 +198,18 @@ class UpdatePropertyFragment : UpdatePropertyAdapter.PhotoDescriptionChanged, Fr
         }
 
         return rootView
+    }
+
+    private fun getCurrentPropertyTypeIndex(
+        propertyTypeSpinner: AppCompatSpinner,
+        s: String?,
+    ): Int {
+        for (i in 0..propertyTypeSpinner.count) {
+            if (propertyTypeSpinner.getItemAtPosition(i).toString().contentEquals(s)) {
+                return i
+            }
+        }
+        return 0
     }
 
     private fun setUpRecyclerView(recyclerView: RecyclerView, mediaList: List<Media>) {
