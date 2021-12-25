7ccfc63f55cb37d3f004651affdb09171ce9a1af
Thierry Bitar <bitarthierry@gmail.com>
2021-12-25 18:57:47 +0100

    Date of sale of the property displayed if the property is sold

--- app/src/main/java/thierry/realestatemanager/ui/propertylist/PropertyListAdapter.kt
+++ app/src/main/java/thierry/realestatemanager/ui/propertylist/PropertyListAdapter.kt
@@ -12,6 +12,7 @@ import com.bumptech.glide.Glide
 import thierry.realestatemanager.R
 import thierry.realestatemanager.model.FullProperty
 import thierry.realestatemanager.utils.Utils
+import java.time.format.DateTimeFormatter
 
 class PropertyListAdapter(
     private val properties: List<FullProperty>,
@@ -58,7 +59,12 @@ class PropertyListAdapter(
             }
         }
 
+        if (item.property.dateOfSale != null) {
+            holder.dateOfSaleOfProperty.text = Utils.epochMilliToLocalDate(item.property.dateOfSale)
+                .format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+        }
         holder.isSoldTextView.isVisible = item.property.isSold == true
+        holder.dateOfSaleOfProperty.isVisible = item.property.isSold == true
 
     }
 
@@ -73,6 +79,7 @@ class PropertyListAdapter(
         val propertyTown: TextView = binding.findViewById(R.id.propertyTown)
         val propertyPhoto: ImageView = binding.findViewById(R.id.propertyPicture)
         val isSoldTextView: TextView = binding.findViewById(R.id.propertyIsSold)
+        val dateOfSaleOfProperty: TextView = binding.findViewById(R.id.date_of_sale_of_property)
     }
 
 }
