5be396ea0544b1d90693b826db872bad543475ae
Thierry Bitar <bitarthierry@gmail.com>
2021-12-27 17:55:37 +0100

    MediaDialogFragment.kt finished

--- app/src/main/java/thierry/realestatemanager/ui/propertydetail/MediaDialogFragment.kt
+++ app/src/main/java/thierry/realestatemanager/ui/propertydetail/MediaDialogFragment.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.MediaController
 import android.widget.VideoView
 import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import thierry.realestatemanager.R
 
 class MediaDialogFragment : DialogFragment() {
@@ -23,29 +24,46 @@ class MediaDialogFragment : DialogFragment() {
 
         val videoview = rootView.findViewById<VideoView>(R.id.videoview)
         val imageview = rootView.findViewById<ImageView>(R.id.imageview)
+        val startButton = rootView.findViewById<FloatingActionButton>(R.id.videoview_start)
+        val pauseButton = rootView.findViewById<FloatingActionButton>(R.id.videoview_pause)
         val mArgs = arguments
         val mediaUri: String? = mArgs!!.getString("key")
 
         if (mediaUri.toString().contains("Photo")) {
             videoview.isVisible = false
             imageview.isVisible = true
-            imageview.setImageURI(mediaUri?.toUri())
+            startButton.hide()
+            pauseButton.hide()
+            Glide.with(imageview)
+                .load(mediaUri)
+                .centerCrop()
+                .into(imageview)
         } else {
             videoview.isVisible = true
             imageview.isVisible = false
+            pauseButton.show()
             videoview.setVideoURI(mediaUri?.toUri())
-            val mediaController = MediaController(videoview.context)
-            videoview.setOnClickListener(View.OnClickListener {
+            pauseButton.setOnClickListener(View.OnClickListener {
+                pauseButton.hide()
+                startButton.show()
+                videoview.pause()
+            })
+            startButton.setOnClickListener(View.OnClickListener {
+                startButton.hide()
+                pauseButton.show()
                 videoview.start()
             })
-
             videoview.setOnPreparedListener {
-                it.setOnVideoSizeChangedListener { mediaPlayer, i, i2 ->
-                    videoview.setMediaController(mediaController)
-                    mediaController.setAnchorView(requireView().rootView)
-                }
                 videoview.start()
-                videoview.pause()
+            }
+            videoview.setOnCompletionListener {
+                if (it.isPlaying) {
+                    startButton.hide()
+                    pauseButton.show()
+                } else {
+                    startButton.show()
+                    pauseButton.hide()
+                }
             }
         }
 
