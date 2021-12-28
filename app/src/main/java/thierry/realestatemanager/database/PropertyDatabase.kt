5a2c057daa04084c83575fcbb64c0b874d530715
Thierry Bitar <bitarthierry@gmail.com>
2021-12-28 14:15:13 +0100

    PropertyDatabase.kt updated

--- app/src/main/java/thierry/realestatemanager/database/PropertyDatabase.kt
+++ app/src/main/java/thierry/realestatemanager/database/PropertyDatabase.kt
@@ -1,5 +1,6 @@
 package thierry.realestatemanager.database
 
+import android.annotation.SuppressLint
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
@@ -23,12 +24,14 @@ abstract class PropertyDatabase : RoomDatabase() {
         private val database: Provider<PropertyDatabase>,
         @ApplicationScope private val applicationScope: CoroutineScope,
     ) : RoomDatabase.Callback() {
+        @SuppressLint("SdCardPath")
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
 
             val dao = database.get().propertyDao()
 
             applicationScope.launch {
+
                 dao.insertProperty(Property(price = 17870000,
                     address = Address(street = "31 Avenue André Morizet",
                         city = "Boulogne Billancourt",
@@ -65,6 +68,19 @@ abstract class PropertyDatabase : RoomDatabase() {
                     dateOfCreation = Utils.getTodayDate(),
                     propertyAgent = "Alex",
                     description = "3How do you know exactly what you're buying when it comes to land? A legal description is a written record of a piece of land containing information that clearly identifies it. This description can be written in a few different forms depending on where the property is located in the United States. However, when dealing with one geographic area, the descriptions will tend to follow the same wording and language.A legal description provides legal evidence of the boundaries and allows a surveyor to accurately determine property lines at a later time. This is incredibly useful and necessary during real estate transactions or disputes. The description will typically appear on sales contracts and the property deed."))
+                dao.insertProperty(Property(price = 22650000,
+                    address = Address(street = "100 Avenue André Morizet",
+                        city = "Boulogne Billancourt",
+                        postcode = 92100, country = "France"),
+                    type = "House",
+                    numberOfRooms = 4,
+                    numberOfBedrooms = 3,
+                    numberOfBathrooms = 3,
+                    surface = 480,
+                    dateOfCreation = Utils.getTodayDate(),
+                    propertyAgent = "Eric",
+                    description = "4How do you know exactly what you're buying when it comes to land? A legal description is a written record of a piece of land containing information that clearly identifies it. This description can be written in a few different forms depending on where the property is located in the United States. However, when dealing with one geographic area, the descriptions will tend to follow the same wording and language.A legal description provides legal evidence of the boundaries and allows a surveyor to accurately determine property lines at a later time. This is incredibly useful and necessary during real estate transactions or disputes. The description will typically appear on sales contracts and the property deed."))
+
                 dao.insertPropertyPointsOfInterest(PointsOfInterest(school = true,
                     university = true,
                     propertyId = 1,
@@ -86,6 +102,41 @@ abstract class PropertyDatabase : RoomDatabase() {
                     sportsClubs = false,
                     stations = true,
                     shoppingCenter = false))
+                dao.insertPropertyPointsOfInterest(PointsOfInterest(school = true,
+                    university = true,
+                    propertyId = 4,
+                    parks = true,
+                    sportsClubs = false,
+                    stations = true,
+                    shoppingCenter = false))
+
+                dao.insertPropertyMedia(Media(propertyId = 1, uri = "/data/user/0/thierry.realestatemanager/files/1Photo1.jpg", description = "Living Room"))
+                dao.insertPropertyMedia(Media(propertyId = 1, uri = "/data/user/0/thierry.realestatemanager/files/1Photo2.jpg", description = "Living Room 2"))
+                dao.insertPropertyMedia(Media(propertyId = 1, uri = "/data/user/0/thierry.realestatemanager/files/1Photo3.jpg", description = "Bedroom"))
+                dao.insertPropertyMedia(Media(propertyId = 1, uri = "/data/user/0/thierry.realestatemanager/files/1Photo4.jpg", description = "Bathroom"))
+
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo1.jpg", description = "Living Room"))
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo2.jpg", description = "Bedroom"))
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo3.jpg", description = "Bedroom 2"))
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo4.jpg", description = "Kitchen"))
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo5.jpg", description = "Bathroom"))
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo6.jpg", description = "Bathroom 2"))
+                dao.insertPropertyMedia(Media(propertyId = 2, uri = "/data/user/0/thierry.realestatemanager/files/2Photo7.jpg", description = "Sauna"))
+
+                dao.insertPropertyMedia(Media(propertyId = 3, uri = "/data/user/0/thierry.realestatemanager/files/3Photo1.jpg", description = "Living Room"))
+                dao.insertPropertyMedia(Media(propertyId = 3, uri = "/data/user/0/thierry.realestatemanager/files/3Photo2.jpg", description = "Living Room 2"))
+                dao.insertPropertyMedia(Media(propertyId = 3, uri = "/data/user/0/thierry.realestatemanager/files/3Photo3.jpg", description = "Living Room 3"))
+                dao.insertPropertyMedia(Media(propertyId = 3, uri = "/data/user/0/thierry.realestatemanager/files/3Photo4.jpg", description = "Kitchen"))
+                dao.insertPropertyMedia(Media(propertyId = 3, uri = "/data/user/0/thierry.realestatemanager/files/3Photo5.jpg", description = "Bedroom"))
+                dao.insertPropertyMedia(Media(propertyId = 3, uri = "/data/user/0/thierry.realestatemanager/files/3Photo6.jpg", description = "Bathroom"))
+
+                dao.insertPropertyMedia(Media(propertyId = 4, uri = "/data/user/0/thierry.realestatemanager/files/4Photo1.jpg", description = "Living Room"))
+                dao.insertPropertyMedia(Media(propertyId = 4, uri = "/data/user/0/thierry.realestatemanager/files/4Photo2.jpg", description = "Kitchen"))
+                dao.insertPropertyMedia(Media(propertyId = 4, uri = "/data/user/0/thierry.realestatemanager/files/4Photo3.jpg", description = "Bedroom"))
+                dao.insertPropertyMedia(Media(propertyId = 4, uri = "/data/user/0/thierry.realestatemanager/files/4Photo4.jpg", description = "Bedroom 2"))
+                dao.insertPropertyMedia(Media(propertyId = 4, uri = "/data/user/0/thierry.realestatemanager/files/4Photo5.jpg", description = "Bathroom"))
+                dao.insertPropertyMedia(Media(propertyId = 4, uri = "/data/user/0/thierry.realestatemanager/files/4Photo6.jpg", description = "Bathroom 2"))
+
             }
         }
     }
